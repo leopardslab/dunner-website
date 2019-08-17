8ecd2d94e7b82e74e9a3ce1ed448c3a47fe1d3bc
apoorvam <app.apoorva@gmail.com>
2019-08-17 01:45:29 -0400

    test: print task file

--- get_recipes.sh
+++ get_recipes.sh
@@ -10,11 +10,12 @@ git clone --depth 1 https://github.com/leopardslab/dunner-cookbook.git $CLONE_TE
 for d in "$CLONE_TEMP_DIR/recipes/*/"; do
 	recipe_name="$(echo $(basename $d))"
 	echo "Copying recipe: $recipe_name"
+	echo "Dunner task file:"
+	cat "$CLONE_TEMP_DIR/recipes/$recipe_name/.dunner.yaml"
 	recipe_content="$(<"$CLONE_TEMP_DIR/recipes/$recipe_name/.dunner.yaml")"
 	echo "Recipe content: $recipe_content"
 	recipe_readme="$CLONE_TEMP_DIR/recipes/$recipe_name/README.md"
 	echo "\n# Recipe\n\n\`\`\`\n$(echo "$recipe_content")\n\`\`\`\n" >> "$recipe_readme"
-	cat $recipe_readme
 	cp "$recipe_readme" "$DEST_DIR/$recipe_name.md"
 done
 
