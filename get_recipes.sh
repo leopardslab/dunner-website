bd77250c8f0597ecbd10514f2347d4e2e6c1e884
apoorvam <app.apoorva@gmail.com>
2019-08-18 01:32:49 -0400

    test: print recipe

--- get_recipes.sh
+++ get_recipes.sh
@@ -12,7 +12,7 @@ for d in "$CLONE_TEMP_DIR/recipes/*/"; do
 	echo "Copying recipe: $recipe_name"
 	echo "Dunner task file:"
 	cat "$CLONE_TEMP_DIR/recipes/$recipe_name/.dunner.yaml"
-	recipe_content="$(<"$CLONE_TEMP_DIR/recipes/$recipe_name/.dunner.yaml")"
+	recipe_content="$(echo "$(cat $CLONE_TEMP_DIR/recipes/$recipe_name/.dunner.yaml)")"
 	echo "Recipe content: $recipe_content"
 	recipe_readme="$CLONE_TEMP_DIR/recipes/$recipe_name/README.md"
 	echo "\n# Recipe\n\n\`\`\`\n$(echo "$recipe_content")\n\`\`\`\n" >> "$recipe_readme"
